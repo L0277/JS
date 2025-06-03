4669eccc50ef9df92e5130a3d8c2fbcfd3f8476e
L0277 <www.lizabhattarai0277@gmail.com>
2025-06-02 19:58:24 -0700

    feat; new code added

--- Demo.js
+++ Demo.js
@@ -1,3 +1,41 @@
 console.log('Project Setup')
 console.log('login page feature')
 console.log("sdfghjkhgfg")
+
+const userData=[
+{
+    name:"Liza 1",
+    age:21,
+    hasPassedOut:false,
+    occupation: undefined,
+},
+{
+    name:"Liza 2",
+    age:22,
+    hasPassedOut:false,
+    occupation:"intern",
+},{
+    name:"Liza 3",
+    age:23,
+    hasPassedOut:true,
+    occupation:"swe",
+}
+]
+
+const apiResponse={
+    statusCode:200,
+    message:"user data eached successfully",
+    data:userData
+}
+
+const getUserName=()=>{
+    if(apiResponse.statusCode===200){
+        const result= apiResponse.data
+        .filter((user)=>user.occupation)
+        .map((user)=> user.name)
+        return result
+    }
+    return[]
+}
+console.log(getUserName())
+
